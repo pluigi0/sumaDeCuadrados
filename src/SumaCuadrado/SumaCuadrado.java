31c77094c39bfbf7a0118c8f028f1ae7577ac8f6
pluig <pluig@LAPTOP-FTUUF4D0>
2021-11-20 19:43:34 -0500

    Suma de cuadrados

--- src/SumaCuadrado/SumaCuadrado.java
+++ src/SumaCuadrado/SumaCuadrado.java
@@ -6,17 +6,16 @@ import java.util.Scanner;
 public class SumaCuadrado {
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
-        float a, b; //variable b is neither read or written
-        float d;
+        float primer, segundo, operacion; 
         
         System.out.println("Please enter the first number: ");
-        a = entrada.nextFloat();
+        primer = entrada.nextFloat();
         System.out.println("Please enter the second number: ");
-        b = entrada.nextFloat();
+        segundo = entrada.nextFloat();
         
         //Elevando a la segunda potencia el atributo a y b, multiplicando 2 por a y b.
-        d = (float) (Math.pow(a,2)+Math.pow(b, 2) + (2*a*b));
+        operacion = (float) (Math.pow(primer,2)+Math.pow(segundo, 2) + (2*primer*segundo));
         
-        System.out.println("aa: "+d);
+        System.out.println("aa: "+operacion);
     }
 }
